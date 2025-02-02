5def4446bcbfff3be75d429a7d8f8d66fc6600bb
Luna <maithetranh@gmail.com>
2025-02-03 02:29:58 +0700

    simple navigate to login

--- AnimeDashboard/src/routes/ProtectedRoute.jsx
+++ AnimeDashboard/src/routes/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../context/AuthContext";
 const ProtectedRoute = () => {
   const { isAuthenticated } = useAuth();
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
